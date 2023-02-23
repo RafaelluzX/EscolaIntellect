e6b93c5e01866796471e064248ed336bd32ce4cd
rafaelluz <rafael.l.s1988@gmail.com>
2023-02-23 20:45:05 -0300

    rotas criadas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import React from "react";
 import Home from "./pages/Home"
 import Login from "./pages/Login";
 import Cursos from "./pages/Cursos"
+import { 
+      BrowserRouter,
+      Routes,
+      Route,
+ } from "react-router-dom";
 
 function App() {
   return (
-    <Cursos/>
-  )
-    
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/Cursos" element={<Cursos/>} />
+      <Route path="/Login" element={<Login/>} />
+    </Routes>
+  </BrowserRouter>
+  );  
 }
 
 export default App;
