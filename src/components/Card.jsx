6bc803a3b5747eac39119a119aad2eabefcbaee8
RafaelluzX <rafael.l.s1988@gmail>
2023-05-24 21:32:45 -0300

    botoes estilizados

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,9 @@ const Card = (props) => {
         Some quick example text to build on the card title and make up the bulk
         of the card's content.
       </p>
-      <a href="#">Acessar</a>
+      <button className="buttonCard"><a href="#">Acessar</a></button>
+      
+      
     </div>
   );
 };
--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -27,13 +27,10 @@ function Cursos() {
       <Navbar />
       <div>
         <section className="presentation-class-courses container mt-4">
-          <h2 className="title-presentation">Quer aprender a programar?</h2>
+          <h2 className="title-presentation">QUER APRENDER A PROGRAMAR DE VERDADE?</h2>
           <p className="description-section">
-            Mussum Ipsum, cacilds vidis litro abertis. Si u mundo tá muito
-            paradis? Toma um mé que o mundo vai girarzis!Não sou faixa preta
-            cumpadi, sou preto inteiris, inteiris.Praesent malesuada urna nisi,
-            quis volutpat erat hendrerit non. Nam vulputate dapibus.A ordem dos
-            tratores não altera o pão duris.{" "}
+          Programar pode parecer desafiador à primeira vista, mas com um pouco de prática e paciência, você verá que é mais fácil do que imagina. A programação é como aprender um novo idioma,
+           onde você usa uma combinação de instruções e lógica para criar algo novo.{" "}
           </p>
         </section>
         <section className="cards-courses container d-flex flex-wrap gap-1 justify-content-center">
